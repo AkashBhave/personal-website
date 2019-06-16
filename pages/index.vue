f7e457d7702c3016e91540ca8757b8544e3ce200
Akash Bhave <akashbbhave@gmail.com>
2019-06-16 18:49:21 -0400

    Add scroll down button to home

--- pages/index.vue
+++ pages/index.vue
@@ -1,41 +1,52 @@
 <template>
-    <section class="h-full">
-        <div class="flex flex-wrap-reverse h-full">
-            <div class="flex flex-col justify-center md:w-1/2 w-full bg-white p-8">
-                <p class="font-bold font-serif text-5xl">Hello.</p>
-                <p class="text-4xl">
-                    My name is
-                    <span class="font-bold">Akash</span>, and I'm a
-                </p>
-                <p class="text-4xl italic">
-                    <vue-typer
-                        :text="meKeywords"
-                        :repeat="Infinity"
-                        :shuffle="true"
-                        initial-action="typing"
-                        :pre-type-delay="70"
-                        :type-delay="60"
-                        :pre-erase-delay="1500"
-                        :erase-delay="250"
-                        erase-style="select-all"
-                        :erase-on-complete="false"
-                        caret-animation="smooth"
-                    ></vue-typer>.
-                </p>
+    <main class="h-full">
+        <section class="h-full">
+            <div class="flex flex-wrap-reverse h-full">
+                <div
+                    class="flex flex-col md:justify-center justify-start md:w-1/2 w-full bg-white p-8"
+                >
+                    <p class="font-bold font-serif text-5xl">Hello.</p>
+                    <p class="text-4xl">
+                        My name is
+                        <span class="font-bold">Akash</span>, and I'm a
+                    </p>
+                    <p class="text-3xl italic">
+                        <vue-typer
+                            class="break-normal whitespace-normal"
+                            :text="meKeywords"
+                            :repeat="Infinity"
+                            :shuffle="true"
+                            initial-action="typing"
+                            :pre-type-delay="70"
+                            :type-delay="60"
+                            :pre-erase-delay="1500"
+                            :erase-delay="250"
+                            erase-style="select-all"
+                            :erase-on-complete="false"
+                            caret-animation="smooth"
+                        ></vue-typer>.
+                    </p>
+                </div>
+                <div
+                    id="hero-picture"
+                    class="flex flex-col justify-end md:w-1/2 w-full bg-blue-primary bg-local bg-contain bg-no-repeat bg-center"
+                ></div>
             </div>
             <div
-                id="home-picture"
-                class="flex flex-col justify-end md:w-1/2 w-full bg-blue-primary bg-local bg-contain bg-no-repeat bg-center"
-            ></div>
-        </div>
-        <div>More content.</div>
-    </section>
+                id="hero-scroll"
+                class="relative h-16 w-16 bg-blue-secondary opacity-100 shadow-md cursor-pointer flex justify-center items-center"
+            >
+                <i class="fas fa-chevron-down text-3xl text-white"></i>
+            </div>
+        </section>
+    </main>
 </template>
 
 <script>
 if (process.browser) {
     var VueTyper = require('vue-typer').VueTyper
 }
+require('@fortawesome/fontawesome-free/css/all.css')
 
 export default {
     components: {
@@ -55,19 +66,25 @@ export default {
 }
 </script>
 
-<style lang="scss" scopred>
-@import '~/assets/scss/_colors.scss';
+<style lang="scss" scoped>
+#hero-picture {
+    background-image: url('~assets/pages/home/me.png');
+    background-blend-mode: overlay;
+}
+#hero-scroll {
+    bottom: 100px;
+    left: 50%;
+    transform: translate(-50%, -50%);
+}
+</style>
 
+<style lang="scss">
+@import '~/assets/scss/_colors.scss';
 .vue-typer .custom.char.selected {
     background-color: $color-primary;
 }
 .vue-typer .custom.caret {
     width: 3px;
     margin-left: 3px;
-}
-
-#home-picture {
-    background-image: url('~assets/pages/home/me.png');
-    background-blend-mode: overlay;
 }
 </style>
