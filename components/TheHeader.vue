4617e62070db30299d62127e193220622039ce5a
Akash Bhave <akashbbhave@gmail.com>
2019-06-15 20:20:14 -0400

    Add links to navbar

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,7 +1,69 @@
 <template>
-    <nav class="navbar" role="navigation"></nav>
+    <nav class="navbar" role="navigation" aria-label="navigation">
+        <a
+            role="button"
+            class="navbar-burger burger"
+            aria-label="menu"
+            aria-expanded="false"
+            data-target="navbar-content"
+        >
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+        </a>
+        <div id="navbar-content" class="navbar-menu">
+            <div class="navbar-start" style="flex-grow: 1; justify-content: center;">
+                <a class="navbar-item">Projects</a>
+                <a class="navbar-item">Blog</a>
+                <a href>
+                    <img
+                        id="logo"
+                        src="~/assets/brand/logo.svg"
+                        alt
+                        class="navbar-item is-hidden-touch"
+                    >
+                </a>
+                <a class="navbar-item">About</a>
+                <a class="navbar-item">Contact</a>
+            </div>
+        </div>
+    </nav>
 </template>
 
 <script>
-export default {}
+export default {
+    mounted() {
+        // Get all "navbar-burger" elements
+        const $navbarBurgers = Array.prototype.slice.call(
+            document.querySelectorAll('.navbar-burger'),
+            0
+        )
+
+        // Check if there are any navbar burgers
+        if ($navbarBurgers.length > 0) {
+            // Add a click event on each of them
+            $navbarBurgers.forEach(el => {
+                el.addEventListener('click', () => {
+                    // Get the target from the "data-target" attribute
+                    const target = el.dataset.target
+                    const $target = document.getElementById(target)
+
+                    // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+                    el.classList.toggle('is-active')
+                    $target.classList.toggle('is-active')
+                })
+            })
+        }
+    }
+}
 </script>
+
+<style lang="scss">
+#logo {
+    height: 60px;
+    padding: 12px;
+    padding-left: 20px;
+    padding-right: 20px;
+}
+</style>
+
