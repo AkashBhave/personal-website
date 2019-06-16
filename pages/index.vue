1300587dc00069f07f5f317b57f1eba93cd76d99
Akash Bhave <akashbbhave@gmail.com>
2019-06-16 16:14:55 -0400

    Add sketch portrait to home

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
     <section class="h-full">
-        <div class="flex h-full">
-            <div class="md:w-1/2 w-full bg-white p-8">
+        <div class="flex flex-wrap-reverse h-full">
+            <div class="flex flex-col justify-center md:w-1/2 w-full bg-white p-8">
                 <p class="font-bold font-serif text-5xl">Hello.</p>
                 <p class="text-4xl">
                     My name is
@@ -9,7 +9,10 @@
                 </p>
                 <p class="text-4xl">web developer.</p>
             </div>
-            <div class="md:w-1/2 w-full bg-blue-primary p-8"></div>
+            <div
+                id="home-picture"
+                class="flex flex-col justify-end md:w-1/2 w-full bg-blue-primary bg-local bg-contain bg-no-repeat bg-center"
+            ></div>
         </div>
         <div>More content.</div>
     </section>
@@ -18,3 +21,12 @@
 <script>
 export default {}
 </script>
+
+<style lang="scss" scoped>
+@import '~/assets/scss/_colors.scss';
+
+#home-picture {
+    background-image: url('~assets/pages/home/me.png');
+    background-blend-mode: overlay;
+}
+</style>
