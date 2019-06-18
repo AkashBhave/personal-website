07d0ed65a7979c78287dcd8fdc119ae1eaacc404
Akash Bhave <akashbbhave@gmail.com>
2019-06-18 18:40:37 -0400

    Serialize Sanity blocks to HTML in blog posts

--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -1,11 +1,13 @@
 <template>
     <main class="container mx-auto p-8">
         <h1 class="font-bold text-4xl font-serif text-center">{{title}}</h1>
+        <block-content :blocks="body"/>
     </main>
 </template>
 
 <script>
 import sanityClient from '~/plugins/sanityClient'
+import BlockContent from 'sanity-blocks-vue-component'
 
 export default {
     async validate({ params }) {
@@ -17,10 +19,16 @@ export default {
             return true
         }
     },
+    components: {
+        BlockContent
+    },
     async asyncData({ params, error }) {
         const postQuery = `*[_type == "post" && slug.current == "${params.slug}"][0]`
         let postData = await sanityClient.fetch(postQuery)
         return postData
+    },
+    data() {
+        return {}
     }
 }
 </script>
