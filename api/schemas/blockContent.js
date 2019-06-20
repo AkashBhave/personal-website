7ef417ad815d257da6c5cdbc47c309b6a17fcbef
Akash Bhave <akashbbhave@gmail.com>
2019-06-20 19:37:19 -0400

    Add image captions and more formats to block content schema

--- api/schemas/blockContent.js
+++ api/schemas/blockContent.js
@@ -27,39 +27,24 @@ export default {
                 { title: 'H3', value: 'h3' },
                 { title: 'H4', value: 'h4' },
                 { title: 'Quote', value: 'blockquote' }
-            ],
-            lists: [{ title: 'Bullet', value: 'bullet' }],
-            // Marks let you mark up inline text in the block editor.
-            marks: {
-                // Decorators usually describe a single property – e.g. a typographic
-                // preference or highlighting by editors.
-                decorators: [
-                    { title: 'Strong', value: 'strong' },
-                    { title: 'Emphasis', value: 'em' }
-                ],
-                // Annotations can be any object structure – e.g. a link or a footnote.
-                annotations: [
-                    {
-                        title: 'URL',
-                        name: 'link',
-                        type: 'object',
-                        fields: [
-                            {
-                                title: 'URL',
-                                name: 'href',
-                                type: 'url'
-                            }
-                        ]
-                    }
-                ]
-            }
+            ]
         },
         // You can add additional types here. Note that you can't use
         // primitive types such as 'string' and 'number' in the same array
         // as a block type.
         {
             type: 'image',
-            options: { hotspot: true }
+            options: { hotspot: true },
+            fields: [
+                {
+                    name: 'caption',
+                    type: 'string',
+                    title: 'Caption',
+                    options: {
+                        isHighlighted: true // <-- make this field easily accessible
+                    }
+                }
+            ]
         },
         {
             type: 'code',
