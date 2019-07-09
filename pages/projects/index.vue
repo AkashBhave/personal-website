19464b554a3ac738bea349b417d1ffd627e158d4
Akash Bhave <akashbbhave@gmail.com>
2019-07-09 16:46:50 -0400

    Use grid layout on projects index

--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -1,20 +1,23 @@
 <template>
     <main>
-        <the-title :title="'Blog'" :subtitle="'Fresh off the CMS.'"></the-title>
+        <the-title
+            :title="'Projects'"
+            :subtitle="'What I\'ve been up to recently (and not so recently).'"
+        ></the-title>
         <div class="container mx-auto px-8 py-12">
             <div class="flex flex-row flex-wrap justify-left -mx-4">
                 <div
-                    v-for="post in posts"
-                    :key="post._id"
-                    class="flex-grow-0 md:w-1/3 w-full whitespace-normal px-4 mb-8"
+                    v-for="project in projects"
+                    :key="project._id"
+                    class="flex-grow-0 md:w-1/2 w-full whitespace-normal px-4 mb-8"
                 >
                     <div class="flex flex-col justify-between cursor-pointer shadow border h-full">
-                        <nuxt-link :to="'/blog/' + post.slug.current">
+                        <nuxt-link :to="'/blog/' + project.slug.current">
                             <no-ssr>
-                                <div v-if="post.mainImage">
+                                <div v-if="project.mainImage">
                                     <img
                                         class="w-full bg-gray-300"
-                                        :src="imageBlocktoURL(post.mainImage)"
+                                        :src="imageBlocktoURL(project.mainImage)"
                                         alt
                                         srcset
                                     />
@@ -26,20 +29,13 @@
                             <div class="p-8 flex flex-col content-start">
                                 <h3
                                     class="font-serif font-bold text-3xl -mb-2 break-words"
-                                >{{post.title}}</h3>
+                                >{{project.title}}</h3>
                                 <h3 class="text-md font-light mt-4">
-                                    Published on
+                                    Updated on
                                     <span
                                         class="text-blue-tertiary"
-                                    >{{ new Date(post._createdAt).toLocaleString() }}</span>
+                                    >{{ new Date(project._updatedAt).toLocaleString() }}</span>
                                 </h3>
-                                <div
-                                    class="w-auto mr-auto text-md font-light text-light py-2 px-4 mt-4 rounded"
-                                    :class="getPostType(post.postType.type).color"
-                                >
-                                    <i class="mr-2" :class="getPostType(post.postType.type).icon"></i>
-                                    <span>{{ getPostType(post.postType.type).name }}</span>
-                                </div>
                             </div>
                         </nuxt-link>
                     </div>
@@ -56,16 +52,14 @@ let builder = imageUrlBuilder(sanityClient)
 
 import TheTitle from '~/components/TheTitle'
 
-import getPostType from '~/plugins/post-type'
-
 export default {
     head() {
         return {
-            title: 'Blog | ' + process.env.SITE_TITLE
+            title: 'Projects | ' + process.env.SITE_TITLE
         }
     },
     async asyncData(context) {
-        return { posts: require('~/static/_data/blog/_.json') }
+        return { projects: require('~/static/_data/projects/_.json') }
     },
     components: {
         TheTitle
@@ -73,15 +67,12 @@ export default {
     methods: {
         imageBlocktoURL(source) {
             if (process.client) {
-                let windowScale = Math.round(window.innerWidth / 3)
+                let windowScale = Math.round(window.innerWidth / 2)
                 return builder
                     .image(source)
                     .width(windowScale)
                     .height(Math.round((windowScale * 9) / 16))
             }
-        },
-        getPostType(type) {
-            return getPostType(type)
         }
     }
 }
