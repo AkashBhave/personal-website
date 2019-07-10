7d3c4f530911d29907f0f1e354c84495883fad14
Akash Bhave <akashbbhave@gmail.com>
2019-07-10 07:48:25 -0400

    Show project links on project page

--- pages/projects/_slug.vue
+++ pages/projects/_slug.vue
@@ -12,22 +12,34 @@
                         </div>
                     </nuxt-link>
                     <div class="flex flex-1 flex-col py-12 px-8 justify-center h-full">
-                        <h1 class="page-title md:text-5xl text-4xl -mb-2">{{ title }}</h1>
-                        <h3 class="text-xl mt-2">
+                        <div class="mb-4">
+                            <span
+                                v-for="link in links"
+                                :key="link"
+                                class="mx-2 w-auto text-xl font-light text-light py-2 px-4 rounded bg-highlight"
+                            >
+                                <a :href="link.url">
+                                    <i class="mr-2" :class="link.iconName"></i>
+                                    <span>{{ link.title }}</span>
+                                </a>
+                            </span>
+                        </div>
+                        <h1 class="page-title md:text-5xl text-4xl mb-4">{{ title }}</h1>
+                        <h3 class="text-xl mb-2">
                             By
                             <span class="italic text-blue-tertiary">Akash Bhave</span>
                         </h3>
-                        <h3 class="mt-2">
+                        <h3 class="mb-4">
                             Updated on
                             <span class="font-mono text-blue-tertiary">{{ createdAt }}</span>
                         </h3>
-                        <div class="mt-4">
+                        <!-- <div class="mt-4">
                             <span
                                 v-for="keyword in keywords"
                                 :key="keyword"
                                 class="inline-block mx-2 font-light p-2 text-light bg-blue-secondary rounded"
                             >{{ keyword }}</span>
-                        </div>
+                        </div>-->
                     </div>
                 </div>
                 <div class="flex items-center lg:w-3/5 w-full bg-gray-200 border">
