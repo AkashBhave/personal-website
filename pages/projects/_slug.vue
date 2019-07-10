db18c9e8e4f4f2d3bec4a6e25296a59473ec909a
Akash Bhave <akashbbhave@gmail.com>
2019-07-09 21:06:21 -0400

    Change some background colors to secondary blue

--- pages/projects/_slug.vue
+++ pages/projects/_slug.vue
@@ -5,7 +5,7 @@
                 <div class="flex flex-col lg:w-2/5 w-full text-center">
                     <nuxt-link :to="'/projects'">
                         <div
-                            class="flex-1 w-full bg-blue-primary text-light font-bold text-uppercase py-4 text-xl shadow"
+                            class="flex-1 w-full bg-blue-secondary text-light font-bold text-uppercase py-4 text-xl shadow"
                         >
                             <i class="fas fa-arrow-left mr-2"></i>
                             <span>Projects</span>
@@ -30,7 +30,7 @@
                         </div>
                     </div>
                 </div>
-                <div class="flex lg:w-3/5 w-full flex-grow bg-gray-200">
+                <div class="flex items-center lg:w-3/5 w-full bg-gray-200 border">
                     <div class="mx-auto">
                         <div v-if="mainImage">
                             <block-content
