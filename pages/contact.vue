0f3a55367517a6102d2fcf20e027b53c512bd27e
Akash Bhave <akashbbhave@gmail.com>
2019-07-11 10:55:47 -0400

    Add rich text editor to contact form

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,5 +1,5 @@
 <template>
-    <main id="contact-form">
+    <main id="contact-form" class="-mx-4">
         <the-title :title="'Contact'" :subtitle="'Get in touch!'"></the-title>
         <form class="lg:w-1/2 w-full container mx-auto px-8 py-12">
             <div class="flex flex-wrap">
@@ -18,11 +18,54 @@
                     <input id="form-subject" type="text" placeholder="What's Up?" />
                 </div>
             </div>
+            <div class="flex flex-wrap">
+                <div class="w-full px-4">
+                    <label for="form-email">Email</label>
+                    <input id="form-email" type="email" placeholder="john.doe@gmail.com" />
+                </div>
+            </div>
+            <div class="flex flex-wrap">
+                <div class="w-full px-4">
+                    <label for="form-message">Message</label>
+                    <no-ssr>
+                        <div class="style-normal">
+                            <editor-content id="form-message" :editor="editor" />
+                        </div>
+                    </no-ssr>
+                    <p class="text-gray-600 text-sm italic">
+                        Supports basic
+                        <a
+                            href="https://www.markdownguide.org/cheat-sheet/#basic-syntax"
+                            class="text-purple-500"
+                        >Markdown</a>.
+                    </p>
+                </div>
+            </div>
         </form>
     </main>
 </template>
 
 <script>
+import { Editor, EditorContent, EditorMenuBar } from 'tiptap'
+import {
+    Blockquote,
+    CodeBlock,
+    HardBreak,
+    Heading,
+    OrderedList,
+    BulletList,
+    ListItem,
+    TodoItem,
+    TodoList,
+    Bold,
+    Code,
+    Italic,
+    Link,
+    Strike,
+    Underline,
+    History
+} from 'tiptap-extensions'
+
 import TheTitle from '~/components/TheTitle'
 
 export default {
@@ -32,25 +75,66 @@ export default {
         }
     },
     components: {
+        EditorMenuBar,
+        EditorContent,
         TheTitle
+    },
+    data() {
+        return {
+            editor: null
+        }
+    },
+    mounted() {
+        this.editor = new Editor({
+            extensions: [
+                new Blockquote(),
+                new CodeBlock(),
+                new HardBreak(),
+                new Heading({ levels: [1, 2, 3] }),
+                new BulletList(),
+                new OrderedList(),
+                new ListItem(),
+                new TodoItem(),
+                new TodoList(),
+                new Bold(),
+                new Code(),
+                new Italic(),
+                new Link(),
+                new Strike(),
+                new Underline(),
+                new History()
+            ],
+            content: "I'd like to talk to you about something."
+        })
+    },
+    beforeDestroy() {
+        this.editor.destroy()
     }
 }
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
 #contact-form {
     label {
         @apply block uppercase tracking-wide text-blue-tertiary font-bold mb-2;
     }
 
-    input {
-        @apply appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 py-4 px-4 mb-6 leading-tight;
+    input,
+    #form-message .ProseMirror {
+        @apply appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 py-3 px-4 mb-6 outline-none border-blue-primary;
 
         &:focus {
             @apply bg-light border-blue-secondary;
         }
     }
+
+    .ProseMirror {
+        margin-bottom: 8px !important;
+
+        p,
+        span {
+            font-size: 16px;
+        }
+    }
 }
 </style>
-
-
