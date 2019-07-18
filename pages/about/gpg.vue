3c5dcb2ab47ff1648e3821a0a03a204b8a74d225
Akash Bhave <akashbbhave@gmail.com>
2019-07-18 16:20:41 -0400

    Fix GPG page, improve link styling

--- pages/about/gpg.vue
+++ pages/about/gpg.vue
@@ -4,15 +4,15 @@
         <div class="container md:w-2/3 w-full mx-auto px-8 py-12">
             <div class="mb-4">
                 <h1 class="text-4xl">Key ID</h1>
-                <block-content-code :code="id" language="plaintext"></block-content-code>
+                <block-content-code :code="{code: id}" language="plaintext"></block-content-code>
             </div>
             <div class="mb-4">
                 <h1 class="text-4xl">Fingerprint</h1>
-                <block-content-code :code="fingerprint" language="plaintext"></block-content-code>
+                <block-content-code :code="{code: fingerprint}" language="plaintext"></block-content-code>
             </div>
             <div class="mb-4">
                 <h1 class="text-4xl">Public Key</h1>
-                <block-content-code :code="publicKey" language="plaintext"></block-content-code>
+                <block-content-code :code="{code: publicKey}" language="plaintext"></block-content-code>
             </div>
         </div>
     </main>
