5308bed8a16850c1524d66dd2886bf683d0c020f
Akash Bhave <akashbbhave@gmail.com>
2019-08-31 21:55:22 -0400

    Remove all files, initialize Lerna project

--- plugins/sanity-image.js
+++
@@ -1,9 +0,0 @@
-import imageUrlBuilder from '@sanity/image-url'
-import sanityClient from '~/plugins/sanity-client'
-let builder = imageUrlBuilder(sanityClient)
-
-export default ({ app }, inject) => {
-    inject('sanityImage', asset => {
-        return builder.image(asset)
-    })
-}
