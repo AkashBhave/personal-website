9252ee174a08514e8b969ecf16bbc87c0152c8b0
Akash Bhave <akashbbhave@gmail.com>
2019-09-01 18:21:57 -0400

    Create footer component

---
+++ web/src/components/footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Logo from '../images/logo.svg'
+import styles from './footer.module.css'
+
+const Footer = () => (
+    <footer id="footer" className={`${styles.footer}`}></footer>
+)
+
+export default Footer
