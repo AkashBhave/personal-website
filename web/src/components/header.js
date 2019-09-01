6b5bf987de6de271722b612bbd1a868421e74dcd
Akash Bhave <akashbbhave@gmail.com>
2019-09-01 18:16:18 -0400

    Apply similar header styling with a few tweaks

--- web/src/components/header.js
+++ web/src/components/header.js
@@ -9,76 +9,44 @@ const Header = () => {
     const [isActive, setActive] = useState(false)
 
     return (
-        <header
-            id="header"
-            className="md:h-20 h-auto md:px-16 px-6 bg-white flex flex-wrap items-center md:py-0 py-6 font-sans w-full border-b-2 border-blue-secondary shadow"
-        >
+        <header id="header" className={`${styles.header} `}>
             <button
                 onClick={() => setActive(!isActive)}
-                className={`hamburger hamburger--collapse ${
+                className={`hamburger hamburger--3dy ${
                     isActive ? 'is-active' : ''
-                } hidden`}
+                } ${styles.hamburgerContainer}`}
                 type="button"
             >
-                <span className="hamburger-box cursor-pointer">
-                    <span className="hamburger-inner"></span>
+                <span className="hamburger-box">
+                    <span
+                        className={`hamburger-inner ${styles.hamburgerInner}`}
+                    ></span>
                 </span>
             </button>
-            <div
-                className={`md:flex md:items-center md:w-auto w-full mx-auto uppercase ${
-                    isActive ? 'block' : 'hidden'
-                }`}
-                id="menu"
-            >
-                <nav>
-                    <ul className="md:flex items-center justify-between text-base text-gray-700 pt-4 md:pt-0">
-                        <li className="md:hidden block hover:text-blue-primary">
-                            <Link className="md:p-4 py-3 px-0 block" to="/">
-                                Home
-                            </Link>
-                        </li>
-                        <li className="hover:text-blue-primary">
-                            <Link
-                                className="md:p-4 py-3 px-0 block"
-                                to="/projects"
-                            >
-                                Projects
-                            </Link>
-                        </li>
-                        <li className="hover:text-blue-primary">
-                            <Link className="md:p-4 py-3 px-0 block" to="/blog">
-                                Blog
-                            </Link>
-                        </li>
-                        <li className="md:block hidden">
-                            <Link to="/">
-                                <img
-                                    className="h-10 px-6 py-1"
-                                    src={Logo}
-                                    alt
-                                    srcset
-                                />
-                            </Link>
-                        </li>
-                        <li className="hover:text-blue-primary">
-                            <Link
-                                className="md:p-4 py-3 px-0 block"
-                                to="/about"
-                            >
-                                About
-                            </Link>
-                        </li>
-                        <li className="hover:text-blue-primary">
-                            <Link
-                                className="md:p-4 py-3 px-0 block md:mb-0 mb-2"
-                                to="/contact"
-                            >
-                                Contact
-                            </Link>
-                        </li>
-                    </ul>
-                </nav>
-            </div>
+            <nav className={`${styles.nav} ${isActive ? 'block' : 'hidden'}`}>
+                <ul>
+                    <li className="only-mobile">
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/projects">Projects</Link>
+                    </li>
+                    <li>
+                        <Link to="/blog">Blog</Link>
+                    </li>
+                    <li className="only-desktop">
+                        <Link to="/">
+                            <img className="h-10 mx-1" src={Logo} alt="Logo" />
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/about">About</Link>
+                    </li>
+                    <li>
+                        <Link to="/contact">Contact</Link>
+                    </li>
+                </ul>
+            </nav>
         </header>
     )
 }
