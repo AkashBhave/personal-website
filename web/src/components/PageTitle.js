954fd72e093cb3ccbe8813fb3487914cc9a11910
Akash Bhave <akashbbhave@gmail.com>
2019-09-01 21:48:32 -0400

    Copy page title component

---
+++ web/src/components/PageTitle.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const PageTitle = props => (
+    <div
+        className="w-full bg-blue-primary text-light px-8 py-12 text-center shadow"
+        id="title"
+    >
+        <h1 className="text-uppercase text-5xl font-bold" title="Title">
+            {props.title}
+        </h1>
+        <h4 className="text-uppercase text-xl font-normal" title="Subtitle">
+            {props.subtitle}
+        </h4>
+    </div>
+)
+
+export default PageTitle
