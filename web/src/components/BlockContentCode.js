7c3e147878e06a78ee9b9b85b58cbd9a1c511f79
Akash Bhave <akashbbhave@gmail.com>
2019-09-02 10:32:16 -0400

    Fix syntax errors in code stylesheet, rename gpg page

--- web/src/components/BlockContentCode.js
+++ web/src/components/BlockContentCode.js
@@ -4,13 +4,10 @@ import PropTypes from 'prop-types'
 import Highlight from 'react-highlight'
 
 const BlockContentCode = props => (
-    <div class="-my-8">
+    <div className="mt-4">
         <Highlight className={props.language || ''}>
-            <p
-                v-if="language"
-                className="text-dark relative hljs-language bg-light font-light px-2 py-1 uppercase border-standard font-sans"
-            >
-                {props.language || 'Unknown'}
+            <p className="text-dark relative hljs-language bg-light font-light px-2 py-1 uppercase border-2 border-blue-primary font-sans">
+                {props.language || 'unknown'}
             </p>
             <code classname={props.language || ''}>{props.code.code}</code>
         </Highlight>
---
+++ web/src/pages/gpg.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import { graphql } from 'gatsby'
+
+import Layout from '../layouts/default'
+import SEO from '../utils/seo'
+import PageTitle from '../components/PageTitle'
+import BlockContentCode from '../components/BlockContentCode'
+
+const AboutGPGPage = ({ data }) => (
+    <Layout>
+        <SEO
+            title="GPG"
+            keywords={[`about`, `gpg`, `gpg info`, `public key`]}
+        />
+
+        <PageTitle title="GPG" subtitle="My online identification." />
+
+        <section class="container md:w-2/3 w-full mx-auto px-8 py-12">
+            <div class="mb-4">
+                <h1 class="text-4xl">Key ID</h1>
+                <BlockContentCode
+                    code={{ code: data.page.gpg.id }}
+                    language="plaintext"
+                />
+            </div>
+            <div class="mb-4">
+                <h1 class="text-4xl">Fingerprint</h1>
+                <BlockContentCode
+                    code={{ code: data.page.gpg.fingerprint }}
+                    language="plaintext"
+                />
+            </div>
+            <div class="mb-4">
+                <h1 class="text-4xl">Public Key</h1>
+                <BlockContentCode
+                    code={{ code: data.page.gpg.publicKey }}
+                    language="plaintext"
+                />
+            </div>
+        </section>
+    </Layout>
+)
+
+export const query = graphql`
+    query AboutGPGPageQuery {
+        page: sanityPageAbout {
+            gpg: _rawGpg
+        }
+    }
+`
+
+export default AboutGPGPage
