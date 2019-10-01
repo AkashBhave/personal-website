f1f2079af71a696c084595604a80d85e0175fdd9
Akash Bhave <akashbbhave@gmail.com>
2019-09-30 21:05:24 -0400

    Update 404 page

--- web/src/pages/404.js
+++ web/src/pages/404.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'gatsby'
 
 import Layout from '../layouts/default'
 import SEO from '../utils/seo'
@@ -7,10 +8,27 @@ function NotFoundPage() {
     return (
         <Layout>
             <SEO title="404 (Not Found)" />
-            <div>
-                <h2 className="bg-yellow-400 text-2xl font-bold inline-block my-8 p-3">
-                    Error: 404 (Not Found)
-                </h2>
+            <div className="flex flex-col items-center justify-center text-center py-12 px-8">
+                <div className="my-4">
+                    <div className="text-highlight text-5xl">
+                        <i className="fas fa-exclamation-circle"></i>
+                    </div>
+                    <div className="text-blue-tertiary">
+                        <h1 className="text-4xl font-bold">Whoops</h1>
+                        <h2 className="text-2xl">
+                            The page you were looking for was not found.
+                        </h2>
+                    </div>
+                </div>
+                <div className="my-4">
+                    <Link
+                        className="transition-standard text-xl text-blue-secondary hover:text-highlight"
+                        to="/"
+                    >
+                        <i className="fas fa-arrow-left mr-2"></i>
+                        <span>Home</span>
+                    </Link>
+                </div>
             </div>
         </Layout>
     )
