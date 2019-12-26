8c1c6a2ea09f2f734c9b75f6d9be5976de998e3d
Akash Bhave <akashbbhave@gmail.com>
2019-12-25 19:58:00 -0500

    Install and configure Sanity dashboard

---
+++ cms/dashboard.config.js
@@ -0,0 +1,16 @@
+export default {
+    widgets: [
+        {
+            name: 'project-info'
+        },
+        {
+            name: 'project-users'
+        },
+        {
+            name: 'document-list',
+            options: {
+                title: 'Recent documents'
+            }
+        }
+    ]
+}
